2bfbcec4435526dfa958c02da5d02147dfc69c73
canunz <ca.nunezy@duocuc.cl>
2023-06-08 20:15:51 -0400

    este es el primer envio

---
+++ listaaaaaaaa.py
@@ -0,0 +1,20 @@
+
+#ejercicio 2
+import numpy as np
+Lista= []
+for _ in range(10):
+    numero_aleatorio= np.random.randint(0,500)
+    Lista.append(numero_aleatorio)
+    if numero_aleatorio % 2 == 0:
+      print(numero_aleatorio)
+Lista.append(501)
+Lista.insert(0,501)
+arreglo=np.array(Lista)
+print("este es el numero maximo:",arreglo.max())
+print(Lista.index(arreglo.max()))
+print(arreglo)
+valor_max=arreglo.max()
+
+for i in range(len(arreglo)):
+   if arreglo[i] == valor_max:
+      print("la posicion del valor maximo es: ", i)
